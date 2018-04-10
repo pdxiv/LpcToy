fb6164edca44aa3594a2087092cc7f8fa461cfbf
pdxiv <frank.stengard@gmail.com>
2018-04-10 10:20:55 +0200

    more imporovements

--- phone_interpolation/get_spelling_to_phone_from_dict.pl
+++ phone_interpolation/get_spelling_to_phone_from_dict.pl
@@ -6,21 +6,28 @@ my $pattern = qr/^(\S+) +(\S+) +(\S+);(\S+) +(\S+) +(\S+)/;
 
 my %translation;
 while (<>) {
-	chomp $_;
-    if ( $_ =~ /$pattern/ ) {
-        # push @{ $translation{$1} }, $4;
-        # push @{ $translation{$2} }, $5;
-        # push @{ $translation{$3} }, $6;
-        $translation{$1}{$4}++;
-        $translation{$2}{$5}++;
-        $translation{$3}{$6}++;
-
+    chomp $_;
+    my @spelling_or_phone = split( /;/, $_ );
+    if ( scalar @spelling_or_phone == 2 ) {
+        my ( @spelling, @phone );
+        @spelling = split( / +/, $spelling_or_phone[0] );
+        @phone    = split( / +/, $spelling_or_phone[1] );
+        if ( scalar @spelling == scalar @phone ) {
+            for ( my $t = 0 ; $t < scalar @spelling ; $t++ ) {
+                if (! exists $translation{ $spelling[$t] }{ $phone[$t] }) {
+                    $translation{ $spelling[$t] }{ $phone[$t] } = 0;
+                }
+                $translation{ $spelling[$t] }{ $phone[$t] }++;
+               # print "$spelling[$t]\t$phone[$t]\n";
+            }
+            # print "\n";
+        }
     }
 }
 
-foreach my $spelling (sort keys %translation) {
-	print "$spelling\n";
-	foreach my $phone (sort keys %{$translation{$spelling}}) {
-		print "    $phone: $translation{$spelling}{$phone}\n";
-	}
+foreach my $spelling ( sort keys %translation ) {
+     print "$spelling\n";
+    foreach my $phone ( sort keys %{ $translation{$spelling} } ) {
+         print "    $phone: $translation{$spelling}{$phone}\n";
+    }
 }
